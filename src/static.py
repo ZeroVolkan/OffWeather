f9549a57f06f35c803c2e1fde9257baf7abc4484
ZeroVolkan <zerovolkan@proton.me>
2025-08-29 23:16:31 +0400

    license

---
+++ src/static.py
@@ -0,0 +1,10 @@
+def apis():
+    from src.open_meteo.api import OpenMeteoAPI, OpenMeteoConfig
+
+    return {
+        "open_meteo": {
+            "class": OpenMeteoAPI,
+            "config": OpenMeteoConfig,
+        },
+        # Add new APIs here
+    }
